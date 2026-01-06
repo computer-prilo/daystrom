560d438c9f4720fddfea111f8f2c8ec01110990d
Lucas Teixeira <prilosac@gmail.com>
2026-01-05 22:43:20 -0800

    updated webfetch tool to handle different requested and response formats and do it's best to give you what you want. no retries included yet

--- src/daystrom/components/tools.py
+++ src/daystrom/components/tools.py
@@ -1,3 +1,5 @@
+from markdownify import markdownify as md
+
 from daystrom.components import tool
 from daystrom.exceptions.components import ToolCallError
 
@@ -15,17 +17,47 @@ def web_fetch(url: str, format: str = "markdown") -> str:
     """
     import httpx
 
-    response = httpx.get(url)
+    accept_header = "*/*"
+    match format:
+        case "text":
+            accept_header = (
+                "text/plain;q=1.0, text/markdown;q=0.9, text/html;q=0.8, */*;q=0.1"
+            )
+        case "html":
+            accept_header = "text/html;q=1.0, application/xhtml+xml;q=0.9, text/plain;q=0.8, text/markdown;q=0.7, */*;q=0.1"
+        case "json":
+            accept_header = "application/json;q=1.0 text/markdown;q=1.0, text/x-markdown;q=0.9, text/plain;q=0.8, text/html;q=0.7, */*;q=0.1"
+        case "markdown":
+            accept_header = "text/markdown;q=1.0, text/x-markdown;q=0.9, text/plain;q=0.8, text/html;q=0.7, */*;q=0.1"
+        case _:
+            # accept_header = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
+            pass
+
+    headers = {
+        "accept": accept_header,
+    }
+    response = httpx.get(url, headers=headers)
     response.raise_for_status()
+    response_type = response.headers.get("content-type")
 
     match format:
         case "text":
-            return response.text
+            ans = response.text
+            if "text/html" in response_type:
+                # TODO: Convert HTML to text
+                pass
+            return ans
         case "html":
             return response.text
         case "json":
-            return response.json()
+            ans = response.text
+            if "application/json" in response_type:
+                ans = response.json()
+            return ans
         case "markdown":
-            return response.text
+            ans = response.text
+            if "text/html" in response_type:
+                ans = md(ans)
+            return ans
         case _:
             raise ToolCallError(f"Unsupported format: {format}")
